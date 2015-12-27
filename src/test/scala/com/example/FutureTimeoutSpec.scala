a04bab97ff1e6660885acd549279958b74d89a6f
ksilin <konstantin.silin@gmail.com>
2015-12-27 11:13:03 +0100

    improving FutureTimeoutSpec

--- src/test/scala/com/example/FutureTimeoutSpec.scala
+++ src/test/scala/com/example/FutureTimeoutSpec.scala
@@ -5,112 +5,110 @@ import org.scalatest._
 
 import scala.concurrent.{Await, Future}
 import scala.util.control.NonFatal
-import scala.util.{Failure, Try}
+import scala.util.{Success, Failure, Try}
 
-class FutureTimeoutSpec extends FunSpec with Matchers {
+class FutureTimeoutSpec extends AsyncFunSpec with Matchers with BeforeAndAfterEach {
 
   import scala.concurrent.duration._
+  import com.example.helpers.FutureTimeoutHelper._
 
   implicit val sys = ActorSystem("test")
   implicit val ec = sys.dispatcher
-  val successful: Future[String] = Future {"hi"}
+
+  val expectedTimeoutMsg: String = "expected timeout"
+  val unexpectedTimeoutMsg: String = "unexpected timeout"
+
+  var resFuture: Future[(String, String)] = _
+
+  override def beforeEach() = {
+    resFuture = for {
+      d <- (Future {"hi"}).orDefault(10 seconds, unexpectedTimeoutMsg)
+      f <- {Future {Thread.sleep(2000); "awake"}}.orDefault(1 second, expectedTimeoutMsg)
+    } yield (d, f)
+  }
 
   describe("Futures combination & timeouts") {
-
-    import com.example.helpers.FutureTimeoutHelper._
 
     // actually, you should not block at all when using futures, so having them time out is not an optimal technique
     // rolling your own timeout: http://stackoverflow.com/questions/16304471/scala-futures-built-in-timeout/16305056#16305056
 
+
     describe("individual timeouts") {
 
-      it("completing a future of list with individual timeouts") {
-
-        val resFuture = for {
-          d <- successful.orDefault(10 seconds, "timed out 1")
-          f <- {Future {Thread.sleep(2000); "awake"}}.orDefault(1 second, "timeout default")
-        } yield (d, f)
-
+      it("completing a future of list with individual timeouts - simple") {
         // leave some allowance - if the timeout of Await is shorter than the timeout of the failed Future, all futures fail
         val res: (String, String) = Await.result(resFuture, 20 seconds)
-        res should be(("hi", "timeout default"))
-      }
-
-      it("completing a future tuple with individual timeout - Either") {
-
-        val resFuture: Future[(String, String)] = for {
-          d <- successful.orDefault(10 seconds, "unexpected timeout")
-          f <- {Future {Thread.sleep(2000); "awake"}}.orDefault(1 second, "expected timeout")
-        } yield (d, f)
-
-        val combinedWithDefault: Future[Either[String, (String, String)]] = resFuture.orEitherDefault(10 seconds, "combined future timed out")
-
-        // if the timeout of Await is shorter than the timeout of the failed Future, all futures fail
-        val res: Either[String, (String, String)] = Await.result(combinedWithDefault, 20 seconds)
-
-        res should be(Right("hi", "expected timeout"))
+        res should be(("hi", expectedTimeoutMsg))
       }
     }
 
-    describe("combination timeout") {
+    describe("common timeout") {
 
-      it("completing a future of list with individual timeouts - common timeout fail") {
-
-        val resFuture = for {
-          d <- successful.orDefault(10 seconds, "timed out 1")
-          f <- {Future {Thread.sleep(2000); "awake"}}.orDefault(1 second, "timeout default")
-        } yield (d, f)
-
+      it("common timeout fail") {
         // without allowance - all futures fail
-        intercept[java.util.concurrent.TimeoutException] {Await.result(resFuture, 1 seconds)}
-      }
-
-      it("completing a future of list with individual timeouts - common timeout fail - either") {
-
-        val resFuture = for {
-          d <- successful.orDefault(10 seconds, "timed out 1")
-          f <- {Future {Thread.sleep(2000); "awake"}}.orDefault(1 second, "timeout default")
-        } yield (d, f)
-
-        val res: Either[String, (String, String)] = try {
-          Right(Await.result(resFuture, 1 seconds))
-        } catch {
-          case NonFatal(ex) => Left(s"failed with $ex")
+        val ex = intercept[java.util.concurrent.TimeoutException] {
+          Await.result(resFuture, 1 seconds)
         }
-        res should be(Left("failed with java.util.concurrent.TimeoutException: Futures timed out after [1 second]"))
+        ex should have message ("Futures timed out after [1 second]") // needs Future[Assertion]
       }
 
-      it("completing a future of list with individual timeouts - common timeout fail - try") {
+      describe("Either") {
 
-        val resFuture = for {
-          d <- successful.orDefault(10 seconds, "timed out 1")
-          f <- {Future {Thread.sleep(2000); "awake"}}.orDefault(1 second, "timeout default")
-        } yield (d, f)
+        it("completing a future of list with individual timeouts - common timeout fail - either") {
+          val res: Either[String, (String, String)] = try {
+            Right(Await.result(resFuture, 1 seconds))
+          } catch {
+            case NonFatal(ex) => Left(s"failed with $ex")
+          }
+          res should be(Left("failed with java.util.concurrent.TimeoutException: Futures timed out after [1 second]"))
+        }
 
-        // Try is basically an error-specific Either
-        val res: Try[(String, String)] = Try {Await.result(resFuture, 1 seconds)}
+        it("completing a future tuple with combined timeout - default") {
+          val combinedWithDefault: Future[Either[String, (String, String)]] =
+            resFuture.orEitherDefault[String](10 seconds, "combined future timed out")
+          // if the timeout of Await is shorter than the timeout of the failed Future, all futures fail
+          val res: Either[String, (String, String)] = Await.result(combinedWithDefault, 20 seconds)
+          // TODO  see failed impl for orEitherDefault
+          res should be(Right("hi", expectedTimeoutMsg))
+        }
 
-        res should be('failure) // isFailure
-        res shouldBe a [Failure[(String, String)]]
-        // yeah, we shoudl actually be matching
-        val x: Throwable = res.asInstanceOf[Failure[(String, String)]].exception
-        x should have message "Futures timed out after [1 second]"
+        it("failing a future tuple with combined timeout - default") {
+          val combinedWithDefault: Future[Either[String, (String, String)]] =
+            resFuture.orEitherDefault[String](1 millisecond, "combined future timed out")
+          // if the timeout of Await is shorter than the timeout of the failed Future, all futures fail
+          val res: Either[String, (String, String)] = Await.result(combinedWithDefault, 20 seconds)
+          res should be(Left("combined future timed out"))
+        }
       }
 
-      it("completing a future tuple with combined timeout try") {
+      describe("Try") {
 
-        val resFuture: Future[(String, String)] = for {
-          d <- successful.orDefault(10 seconds, "unexpected timeout")
-          f <- {Future {Thread.sleep(2000); "awake"}}.orDefault(1 second, "expected timeout")
-        } yield (d, f)
+        it("common timeout fail") {
+          // Try is basically an error-specific Either
+          val res: Try[(String, String)] = Try {
+            Await.result(resFuture, 1 seconds)
+          }
 
-        val combinedWithDefault: Future[Either[String, (String, String)]] = resFuture.orEitherDefault(10 seconds, "combined future timed out")
+          res should be('failure) // isFailure
+          res shouldBe a[Failure[(String, String)]]
+          // yeah, we should actually be matching
+          val x: Throwable = res.asInstanceOf[Failure[(String, String)]].exception
+          x should have message "Futures timed out after [1 second]"
+        }
 
-        // if the timeout of Await is shorter than the timeout of the failed Future, all futures fail
-        val res: Either[String, (String, String)] = Await.result(combinedWithDefault, 20 seconds)
+        it("common timeout success") {
 
-        res should be (Right("hi", "expected timeout"))
+          val res: Try[(String, String)] = Try {
+            Await.result(resFuture, 10 seconds)
+          }
+          res shouldNot be('failure) // isFailure
+          res shouldBe a[Success[(String, String)]]
+          // if the timeout of Await is shorter than the timeout of the failed Future, all futures fail
+
+          res should be(Success("hi", expectedTimeoutMsg))
+        }
       }
+    }
 
       // TODO - try scalaz disjunction (\/) and Validation
 
@@ -118,6 +116,5 @@ class FutureTimeoutSpec extends FunSpec with Matchers {
 
       // TODO - combining Future & Either from cats: http://eed3si9n.com/herding-cats/stacking-future-and-either.html
 
-    }
   }
 }
--- src/test/scala/com/example/helpers/FutureTimeoutHelper.scala
+++ src/test/scala/com/example/helpers/FutureTimeoutHelper.scala
@@ -7,7 +7,7 @@ import scala.concurrent.Future
 
 object FutureTimeoutHelper {
 
-//  http://stackoverflow.com/questions/17466889/run-multiple-futures-in-parallel-return-default-value-on-timeout
+  //  http://stackoverflow.com/questions/17466889/run-multiple-futures-in-parallel-return-default-value-on-timeout
 
   implicit class FutureWithTimeout[T](val f: Future[T]) extends AnyVal {
 
@@ -19,15 +19,22 @@ object FutureTimeoutHelper {
       Future firstCompletedOf Seq(f, delayed)
     }
 
-    def orEitherDefault[U](t: Timeout, default: => U)(implicit system: ActorSystem): Future[Either[U, T]]  = {
+    // TODO - cant get it to work . matches Strign and (String, String) to U
+    // TODO - ask at SUG
+    def orEitherDefault[U](t: Timeout, default: => U)(implicit system: ActorSystem): Future[Either[U, T]] = {
       implicit val ec = system.dispatcher
       val delayed = after(t.duration, system.scheduler)(Future.successful(default))
       val completed: Future[Any] = Future firstCompletedOf Seq(f, delayed)
-      val res: Future[Either[U, T]] = completed match {
-        case succ: Future[T] => succ.map(Right(_))
-        case dflt: Future[U] => dflt.map(Left(_))
+      val res = completed flatMap {
+        case dflt: U => Future {
+          Left(dflt)
+        }
+        case succ: T => Future {
+          Right(succ)
+        }
       }
       res
     }
   }
+
 }
