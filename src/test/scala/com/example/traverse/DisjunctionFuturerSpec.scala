8be6a461667241266ccce58530e0224dade17fc7
ksilin <konstantin.silin@gmail.com>
2016-06-08 07:24:33 +0200

    getting an error in DisjunctionFutureSpec - investigate

--- src/test/scala/com/example/traverse/DisjunctionFuturerSpec.scala
+++ src/test/scala/com/example/traverse/DisjunctionFuturerSpec.scala
@@ -4,6 +4,7 @@ import com.typesafe.scalalogging.LazyLogging
 import org.scalatest.{FunSpec, Matchers}
 
 import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 import scalaz._
 
 class DisjunctionFuturerSpec extends FunSpec with Matchers with LazyLogging {
@@ -14,6 +15,7 @@ class DisjunctionFuturerSpec extends FunSpec with Matchers with LazyLogging {
 
     it("should provide correct Xor"){
       import scalaz.{Functor, \/}
+      import scalaz.Scalaz._
 
       val f = \/.right(Future.successful(1))
       val futurized: Future[\/[Int, Int]] = Functor[Future].counzip(f)
@@ -26,18 +28,25 @@ class DisjunctionFuturerSpec extends FunSpec with Matchers with LazyLogging {
 
     it("same with cats"){
       import cats.data.Xor
-//      import cats.std.future._
+      import cats.implicits._
+      import cats.std.future._
       import cats.syntax.bitraverse._
+      import cats.syntax._
 
       val f: Xor[Future[Int], Future[String]] = Xor.left(Future.successful(1))
       // bisequence works on applicatives - NestedBitraverseOps in NesterTraversesyntax
       // cats.syntax.bitraverse.scala
-      val getX: Future[Xor[Int, String]] = f.bisequence
 
-      getX map { x: Xor[Int, String] =>
-        println(x)
-        x should be(Xor.left(1))
-      }
+      // TODO - why?
+      // Error:(39, 46) value bisequence is not a member of cats.data.Xor[scala.concurrent.Future[Int],scala.concurrent.Future[String]]
+//      val getX: Future[Xor[Int, String]] = f.bisequence
+//      ^
+//      val getX: Future[Xor[Int, String]] = f.bisequence
+
+//      getX map { x: Xor[Int, String] =>
+//        println(x)
+//        x should be(Xor.left(1))
+//      }
     }
   }
 }
