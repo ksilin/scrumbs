1e6f577eb72e255d0bbf3868b17f3e78dc743f50
ksilin <konstantin.silin@gmail.com>
2016-06-10 13:46:56 +0200

    added a simple etl impl with free

---
+++ src/test/scala/com/example/free/FreeKEtlSpec.scala
@@ -0,0 +1,72 @@
+package com.example.free
+
+import org.scalatest.{FunSpec, Matchers}
+
+class FreeKEtlSpec extends FunSpec with Matchers {
+
+  // http://perevillega.com/freek-and-free-monads
+  // https://github.com/ProjectSeptemberInc/freek
+
+  describe("free monads in an etl application") {
+
+    type Symbol = String
+    type Response = String
+    case class Record(id: String, payload: String)
+
+    sealed trait Ops[A]
+    case class Fetch(offset: Int, amount: Int) extends Ops[List[Record]]
+    case class Store(recs: List[Record]) extends Ops[Response]
+    case class CalcBatches(offset: Int, amount: Int, batchSize: Int) extends Ops[List[Fetch]]
+
+    import cats.free.Free
+    import cats.free.Free._
+
+    // turning our Fetch and Store into sth we can run
+    type OpsF[A] = Free[Ops, A]
+
+    // mapping our case classes to instances of Free
+    def fetch(offset: Int, amount: Int): OpsF[List[Record]] = liftF[Ops, List[Record]](Fetch(offset, amount))
+    def store(recs: List[Record]): OpsF[Response] = liftF[Ops, Response](Store(recs))
+    def calcBatches(offset: Int, amount: Int, batchSize: Int): OpsF[List[Fetch]] = liftF[Ops, List[Fetch]](CalcBatches(offset, amount, batchSize))
+
+    it("should allow chaining ops") {
+
+      val simpleEtl: Free[Ops, Response] = fetch(0, 100).flatMap(r => store(r))
+      println(simpleEtl)
+
+      import cats.std.list._
+      import cats.syntax.traverse._
+
+      val etl: Free[Ops, List[Response]] = for {
+        b <- calcBatches(0, 1000, 10)
+        rs <- b.traverseU(f => fetch(f.offset, f.amount).flatMap(r => store(r)))
+      } yield rs
+      println(etl)
+
+      // now we need an interpreter (natural transformation) to actually run things
+      // use an interpreter to obtain a monad with yout aresult (option, future, xor)
+
+      // Id is the simplest monad - effect of having no effect
+      // ~> is syntax sugar for natural transformation
+      import cats.{Id, ~>}
+
+      def etlPrinter: Ops ~> Id =
+        new (Ops ~> Id) {
+          def apply[A](fa: Ops[A]): Id[A] = fa match {
+            case Fetch(offset, amount) =>
+              println(s"fetching $amount records with offset $offset")
+              // IntelliJ is not amused: Expression of type String does not conform to expected type cats.Id[A]
+              List(Record(offset.toString, "fake"))
+            case Store(recs) =>
+              println(s"storing $recs")
+              "ok"
+            case CalcBatches(offset, amount, batchSize) =>
+              println(s"calculating batches: $offset, $amount, $batchSize")
+              List(Fetch(1, 50), Fetch(2, 60))
+          }
+        }
+      // execute by invoking foldMap
+      etl.foldMap(etlPrinter)
+    }
+  }
+}
