11d0a2591c1726b756dec49fe9a3eb7152822401
ksilin <konstantin.silin@gmail.com>
2016-07-25 18:01:45 +0200

    fixed cats.bisequence compiel error in DijunctionFutureSpec

--- src/test/scala/com/example/traverse/DisjunctionFutureSpec.scala
+++ src/test/scala/com/example/traverse/DisjunctionFutureSpec.scala
@@ -5,16 +5,19 @@ import org.scalatest.{FunSpec, Matchers}
 
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
-import scalaz._
 
-class DisjunctionFuturerSpec extends FunSpec with Matchers with LazyLogging {
+class DisjunctionFutureSpec extends FunSpec with Matchers with LazyLogging {
 
   // http://stackoverflow.com/questions/37327257/how-to-transform-xor-of-future-to-future-of-xor
 
   describe("monad transformation as explained by unserscore") {
 
-    it("should provide correct Xor"){
+    import scala.concurrent.Future
+    import scala.concurrent.ExecutionContext.Implicits.global
+
+    it("should provide correct Xor") {
       import scalaz.{Functor, \/}
+      import scalaz._
       import scalaz.Scalaz._
 
       val f = \/.right(Future.successful(1))
@@ -25,28 +28,33 @@ class DisjunctionFuturerSpec extends FunSpec with Matchers with LazyLogging {
         x should be(-\/(1))
       }
     }
+  }
+
+    describe("cats"){
 
     it("same with cats"){
       import cats.data.Xor
-      import cats.implicits._
       import cats.std.future._
       import cats.syntax.bitraverse._
-      import cats.syntax._
+      import cats.Bitraverse
 
-      val f: Xor[Future[Int], Future[String]] = Xor.left(Future.successful(1))
+//      val f: Xor[Future[Int], Future[String]] = Xor.left(Future.successful(1))
+      val f: Xor[Future[Int], Future[String]] = Xor.right(Future.successful("x"))
       // bisequence works on applicatives - NestedBitraverseOps in NesterTraversesyntax
-      // cats.syntax.bitraverse.scala
+//       cats.syntax.bitraverse.scala
 
-      // TODO - why?
-      // Error:(39, 46) value bisequence is not a member of cats.data.Xor[scala.concurrent.Future[Int],scala.concurrent.Future[String]]
-//      val getX: Future[Xor[Int, String]] = f.bisequence
-//      ^
-//      val getX: Future[Xor[Int, String]] = f.bisequence
+      val bt = implicitly[Bitraverse[Xor]]
+      println(bt)
+      val flipped = bt.bisequence(f)
+      println(s"flipped: $flipped")
 
-//      getX map { x: Xor[Int, String] =>
-//        println(x)
-//        x should be(Xor.left(1))
-//      }
+      def flip[A, B](x: Xor[Future[A], Future[B]]): Future[Xor[A, B]] = x.bisequence
+      val getX: Future[Xor[Int, String]] = flip(f)
+
+      getX map { x: Xor[Int, String] =>
+        println(x)
+        x should be(Xor.left(1))
+      }
     }
   }
 }
