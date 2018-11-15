24fa4db8c5062727654350e45ff779467873207b
ksilin <konstantin.silin@gmail.com>
2018-11-15 10:25:15 +0100

    complete MonixTaskFromFutureSpec

--- src/test/scala/com/example/task/MonixTaskFromFutureSpec.scala
+++ src/test/scala/com/example/task/MonixTaskFromFutureSpec.scala
@@ -3,6 +3,8 @@ package com.example.task
 import java.io.Serializable
 import java.time.LocalDateTime
 
+import cats.effect.IO
+import monix.catnap.FutureLift
 import monix.execution.Cancelable
 import monix.execution.schedulers.TestScheduler
 import org.scalatest.{FreeSpec, Matchers, MustMatchers}
@@ -18,6 +20,8 @@ import scala.util.{Failure, Success}
 
 class MonixTaskFromFutureSpec extends FreeSpec with MustMatchers with ScalaFutures with IntegrationPatience{
 
+  // https://monix.io/blog/2018/11/08/tutorial-future-to-task.html
+
   val msc = monix.execution.Scheduler.Implicits.global
 
   def sum(numbers: List[Int])(implicit ec: ExecutionContext): Future[Long] = {
@@ -30,16 +34,28 @@ class MonixTaskFromFutureSpec extends FreeSpec with MustMatchers with ScalaFutur
 
     "simple task conversion" in {
 
-      // this may not be what we want since the processing here is eager -
-      // not apure fn, side fx are not suspended
+      // this may not be what we want since the processing here is strict -
+      // not a pure fn, side fx are not suspended
       def sumTask(num: List[Int])(implicit ec: ExecutionContext): Task[Long] = Task.fromFuture(sum(num))
 
       val t: Task[Long] = sumTask(l)
       val s = t.runToFuture.futureValue
       s mustBe 6L
-      
+
       // what we can do instead is to suspend explicitly:
       def sumTaskSuspend(num: List[Int])(implicit ec: ExecutionContext): Task[Long] = Task.suspend { Task.fromFuture(sum(num))}
+
+      // a shortcut to suspend / fromFuture
+      def sumTaskDefer(num: List[Int])(implicit ec: ExecutionContext): Task[Long] = Task.deferFuture(sum(num))
+
+      // injects scheduler on exec, not invocation
+      def sumTaskAction(num: List[Int]): Task[Long] = Task.deferFutureAction{ implicit scheduler => sum(num)}
+
+     def sumTaskLift(num: List[Int])(implicit ec: ExecutionContext): Task[Long] = FutureLift.from(Task(sum(num)))
+
+      def sumTaskLiftDefer(num: List[Int]): Task[Long] = Task.deferAction{ implicit s => FutureLift.from(Task(sum(num)))}
+
+      def sumIOLift(num: List[Int])(implicit ec: ExecutionContext): IO[Long] = FutureLift.from(IO(sum(num)))
     }
 
   }
